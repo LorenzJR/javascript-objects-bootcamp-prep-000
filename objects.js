fc0c995ee2cd16a2aea5676fc9f2e271de4dd2e8
Lorenz Jahn <jahnl@mskcc.org>
2018-02-17 03:27:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ function updatePlaylist(obj, artist, song) {
   return Object.assign({}, obj, { [artist]: song})
 }
 
-function removeFromPlaylist(playlist, artist) {
- return delete playlist[artist]
+function removeFromPlaylist(obj, artist) {
+ delete obj[artist]
+ return obj
 }
